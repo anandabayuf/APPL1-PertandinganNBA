2075f0b1e0c40d31134361269db699707f8ab9d3
Ananda Bayu <48423130+anandabayuf@users.noreply.github.com>
2020-05-05 23:03:39 +0700

    Add files via upload

---
+++ PertandinganNBA/test/src/aspect/LoggingAspect.java
@@ -0,0 +1,45 @@
+package aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	private final int play = 10000;
+	private final int breakHalf = 10000;
+	private final int breakQuarter = 5000;
+	@Before("execution(public String get*QuarterScore())")
+	public void playing() {
+		System.out.println("\nplaying...\n");
+		try {
+			Thread.sleep(play);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Before("execution(public String getSecondQuarterScore()) || execution(public String getFourthQuarterScore())")
+	public void BreakQuarter() {
+		System.out.println("\nbreak quarter time..");
+		try {
+			Thread.sleep(breakQuarter);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Before("execution(public String getThirdQuarterScore())")
+	public void BreakHalf() {
+		System.out.println("\nbreak half time..");
+		try {
+			Thread.sleep(breakHalf);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
