9c6f1b6564bf11d9b6b8c23f0a0974e8eda9f205
Ananda Bayu <48423130+anandabayuf@users.noreply.github.com>
2020-05-05 23:17:58 +0700

    Add files via upload

---
+++ PertandinganNBA/src/model/PertandinganNBA.java
@@ -0,0 +1,77 @@
+package model;
+
+public class PertandinganNBA {
+   private String date;
+   private String homeTeam;
+   private String awayTeam;
+   private String finalScore;
+   private String firstQuarterScore;
+   private String secondQuarterScore;
+   private String thirdQuarterScore;
+   private String fourthQuarterScore;
+   
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getHomeTeam() {
+		return homeTeam;
+	}
+	
+	public void setHomeTeam(String homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+	
+	public String getAwayTeam() {
+		return awayTeam;
+	}
+	
+	public void setAwayTeam(String awayTeam) {
+		this.awayTeam = awayTeam;
+	}
+	
+	public String getFinalScore() {
+		return finalScore;
+	}
+	
+	public void setFinalScore(String finalScore) {
+		this.finalScore = finalScore;
+	}
+	
+	public String getFirstQuarterScore() {
+		return firstQuarterScore;
+	}
+	
+	public void setFirstQuarterScore(String firstQuarterScore) {
+		this.firstQuarterScore = firstQuarterScore;
+	}
+	
+	public String getSecondQuarterScore() {
+		return secondQuarterScore;
+	}
+	
+	public void setSecondQuarterScore(String secondQuarterScore) {
+		this.secondQuarterScore = secondQuarterScore;
+	}
+	
+	public String getThirdQuarterScore() {
+		return thirdQuarterScore;
+	}
+	
+	public void setThirdQuarterScore(String thirdQuarterScore) {
+		this.thirdQuarterScore = thirdQuarterScore;
+	}
+	
+	public String getFourthQuarterScore() {
+		return fourthQuarterScore;
+	}
+	
+	public void setFourthQuarterScore(String fourthQuarterScore) {
+		this.fourthQuarterScore = fourthQuarterScore;
+	}
+	
+}
